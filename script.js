5a84576380e0d984dc643f085811399050e29ff7
Ashley Mical <ashleymmical@gmail.com>
2023-07-26 16:27:30 -0500

    added color picker adn ability to change .square color upon click

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 document.addEventListener("DOMContentLoaded", createGrid);
 
+let currentColor = "#000"; // Default color (black)
+
 function createGrid() {
     const container = document.getElementById("container");
 
     for (let i = 0; i < 16 * 16; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
+        square.addEventListener("click", colorSquare); // Add click event listener
         container.appendChild(square);
     }
+
+    // Add event listener to the color picker
+    const colorPicker = document.getElementById("colorPicker");
+    colorPicker.addEventListener("change", updateColor);
+}
+
+function colorSquare(event) {
+    // Set the background color permanently on click
+    event.target.style.backgroundColor = currentColor;
+}
+
+function updateColor(event) {
+    currentColor = event.target.value;
 }
